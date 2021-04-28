757074603b0b424875de9e9dcc8aac5cc5922fbe
Nacho <i.vallejos01@ufromail.cl>
2021-04-28 10:43:10 -0400

    Ultimo avance y merch

--- Main/src/main/java/Taller02UnitTesting.java
+++ Main/src/main/java/Taller02UnitTesting.java
@@ -1,4 +1,5 @@
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -7,12 +8,22 @@ public class Taller02UnitTesting {
     public static void main(String[] args) {
         System.out.println("Bienvenido");
         String archivo = "C:\\Users\\Asus\\Desktop\\cosas de progra\\taller2\\cadenas.txt";
+        //metodo creado para saber si el archivo existe
+        validarArchivo(archivo);
+        //lee y analiza los datos del archivo
         lecturaArchivo(archivo);
+        //crea un ArrayList para almacenar las lineas del archivo
         ArrayList<String> interno = lecturaArchivo(archivo);
         //para print del contenido en la pantalla
         // muestraDeLineas(interno);
         System.out.println("la cantidad de lineas es " + interno.size());
-        contarPalindromos(interno);
+        //no terminado
+        //contarPalindromos(interno);
+    }
+
+    public static boolean validarArchivo(String ruta) {
+        File file = new File(ruta);
+        return file.isFile();
     }
 
     private static ArrayList<String> lecturaArchivo(String archivo) {
@@ -45,8 +56,10 @@ public class Taller02UnitTesting {
     }
 
     private static void contarPalindromos(ArrayList<String> interno) {
+        String[] palindromo;
         for (int i = 0; i > interno.size() ; i++) {
-
+             palindromo = interno.get(0).split(";");
         }
+
         }
     }
