8b2bf7dc2592aed18d815f7e8308bde0d241ba4d
Nacho <i.vallejos01@ufromail.cl>
2021-04-28 10:02:48 -0400

    Avance 10:00

--- Main/src/main/java/Taller02UnitTesting.java
+++ Main/src/main/java/Taller02UnitTesting.java
@@ -1,4 +1,6 @@
+import java.io.BufferedReader;
 import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class Taller02UnitTesting {
@@ -6,18 +8,37 @@ public class Taller02UnitTesting {
         System.out.println("Bienvenido");
         String archivo = "C:\\Users\\Asus\\Desktop\\cosas de progra\\taller2\\cadenas.txt";
         lecturaArchivo(archivo);
+        ArrayList<String> interno = lecturaArchivo(archivo);
+        muestraDeLineas(interno);
     }
-    private static void lecturaArchivo(String archivo){
+
+
+    private static ArrayList<String> lecturaArchivo(String archivo) {
         ArrayList<String> lineas = new ArrayList<>();
         String cadenas;
-        FileReader f = null ;
-
+        FileReader fr;
+        try {
+            fr = new FileReader(archivo);
+            BufferedReader br = new BufferedReader(fr);
+            while ((cadenas = br.readLine()) != null) {
+                lineas.add(cadenas);
+            }
+            br.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return lineas;
     }
 
-    private void LeerPalindromo(){
-
+    private static void muestraDeLineas(ArrayList<String> interno) {
+        System.out.println("=======================");
+        for (int i = 0; i < interno.size(); i++) {
+            String[] contenido = interno.get(i).split(";");
+            if (i!=interno.size()-1)
+                System.out.println(""+contenido[0]+", ");
+            else
+                System.out.println(""+contenido[0]);
+        }
+        System.out.println("=======================");
     }
-
-
-
 }
