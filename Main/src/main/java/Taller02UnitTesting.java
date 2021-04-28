d86baf3534f5d330b8b13d1e309347cee8804442
Nacho <i.vallejos01@ufromail.cl>
2021-04-28 09:32:44 -0400

    creando lectura

--- Main/src/main/java/Taller02UnitTesting.java
+++ Main/src/main/java/Taller02UnitTesting.java
@@ -1,8 +1,16 @@
+import java.io.FileReader;
+import java.util.ArrayList;
+
 public class Taller02UnitTesting {
     public static void main(String[] args) {
-
+        System.out.println("Bienvenido");
+        String archivo = "C:\\Users\\Asus\\Desktop\\cosas de progra\\taller2\\cadenas.txt";
+        lecturaArchivo(archivo);
     }
-    private static void menu(){
+    private static void lecturaArchivo(String archivo){
+        ArrayList<String> lineas = new ArrayList<>();
+        String cadenas;
+        FileReader f = null ;
 
     }
 
